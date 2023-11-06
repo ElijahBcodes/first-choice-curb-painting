4c1e3c3e08349f6a52b211e82bf1f60abcba32e0
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2023-11-05 17:46:16 -0800

    Deleted excess components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <div>
+    <div className="flex flex-row">
     <div className="hero " style={{backgroundImage: 'url(https://images.unsplash.com/photo-1602740027538-35973ec88b9e?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGFpbnQlMjByb2xsZXJ8ZW58MHx8MHx8fDA%3D)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
@@ -20,19 +20,20 @@ export default function Home() {
     </div>
   </div>
 </div>
+
 <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="https://images.unsplash.com/photo-1542850513-59adaf9b9b35?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHNwcmF5JTIwcGFpbnR8ZW58MHx8MHx8fDA%3D"  alt="Shoes" /></figure>
+  <figure><img src="https://images.unsplash.com/photo-1601150169741-298168d2831c?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGN1cmJ8ZW58MHx8MHx8fDA%3D"  alt="Shoes" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Paint Address on Curb</h2>
+    <h2 className="card-title">Why Paint Address on Curb?</h2>
     <p>Enhance safety, elevate aesthetics, and make your home easily identifiable with our expertly crafted curb paintings. Unveil the charm of your address with our bespoke designs and personalized service. Illuminate your curb with our professional touch!"
 </p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
     </div>
   </div>
 </div>
-<h1>Why Curb Painting</h1>
+
 </div>
+
     </>
   )
 }
